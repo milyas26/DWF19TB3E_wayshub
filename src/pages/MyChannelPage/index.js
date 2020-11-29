1574bc813eab3bb76a4098a89f7bd1c47c10d0c2
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-11-29 11:15:20 +0700

    add functionalities

--- src/pages/MyChannelPage/index.js
+++ src/pages/MyChannelPage/index.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import BackgroundImage from '../../assets/images/background-youtube.png'
+import User from '../../assets/images/user2.png'
 import MainComponent from '../../components/molecules/MainComponent'
-
 import MyVideos from '../../components/molecules/MyVideos'
 import MyDescription from '../../components/molecules/MyVideos/Description'
-import Profile from '../../components/molecules/Profile'
 import './MyChannel.css'
 
 const MyChannel = () => {
@@ -26,7 +25,20 @@ const MyChannel = () => {
             <img src={BackgroundImage} alt="" />
           </div>
 
-          <Profile to="/edit-channel" title="Edit Channel" />
+          <div className="profile">
+            <div className="user-profile">
+              <img src={User} alt="" />
+              <div className="channel-name">
+                <h6>Channel Saya</h6>
+                <p>236K Subscriber</p>
+              </div>
+              <div className="button-wrapper">
+                <Link to={'/edit-channel'}>
+                  <button className="button btn-sm">Edit Channel</button>
+                </Link>
+              </div>
+            </div>
+          </div>
 
           <div className="link-profile-wrapper">
             <Link className="link-profile" onClick={handleVideo}>
